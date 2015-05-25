f45ea0d9bddaed4680e56d5d7088baf4007b1e96
josethomazini <jose.thomazini@gmail.com>
2015-05-25 00:12:26 -0300

    configurações de acordo com o heroku docs

--- helper/helper/settings/prod.py
+++ helper/helper/settings/prod.py
@@ -1,17 +1,25 @@
 # coding: utf-8
 
+import os
+import dj_database_url
 from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd2m32uq5dd4u2h',
-        'USER': 'yuxlijolyzxwaj',
-        'PASSWORD': 'i_iy0e1GSAkVOM5eD8OYkXDRTm',
-        'HOST': 'ec2-107-21-114-132.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
