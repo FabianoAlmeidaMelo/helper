e2837fae7f8cb90c08eb1ba1a7b335d0fb5b43e1
josethomazini <jose.thomazini@gmail.com>
2015-05-25 00:04:37 -0300

    configs de banco de prod

--- helper/helper/settings/prod.py
+++ helper/helper/settings/prod.py
@@ -8,10 +8,10 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'helper',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': 'd2m32uq5dd4u2h',
+        'USER': 'yuxlijolyzxwaj',
+        'PASSWORD': 'i_iy0e1GSAkVOM5eD8OYkXDRTm',
+        'HOST': 'ec2-107-21-114-132.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
