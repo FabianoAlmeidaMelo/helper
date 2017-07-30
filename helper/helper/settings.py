b3d366a49dcdb07094d9d1011a0541cfbd4cc32b
Fabiano Almeida <fabiano@znc.com.br>
2017-07-30 12:14:19 -0300

    ref #1 settings: adiciona email no ADMINS; comenta o link 'Cadastrar' no base e remove da home

--- helper/helper/settings.py
+++ helper/helper/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+ADMINS = (
+    ('Admin Control H', 'falmeidamelo@uol.com.br'),
+)
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 # ALLOWED_HOSTS = ['.localhost']
