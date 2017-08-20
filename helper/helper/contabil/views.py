69d933e581b794b86825ff12678bff896314729a
Fabiano Almeida <fabiano@znc.com.br>
2017-08-20 14:11:11 -0300

    ref #35 Se a conta não tem contador, não deve ter acesso

--- helper/helper/contabil/views.py
+++ helper/helper/contabil/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from helper.agenda.models import Conta
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-
+from django.http import Http404
 
 @login_required
 def contador_leitura(request, conta_pk):
     conta = get_object_or_404(Conta, id=conta_pk)
+    if not conta.contador:
+    	raise Http404
     contador = conta.contador
 
     context = {}
