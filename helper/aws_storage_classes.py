9fca15c4a96520d523d022388ec3e212d71802ea
Fabiano Almeida <fabiano@znc.com.br>
2017-09-25 22:58:41 -0300

    ref #40 reescreve o init das classes StaticStorage e MediaStorage

--- helper/aws_storage_classes.py
+++ helper/aws_storage_classes.py
@@ -3,26 +3,26 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 # ???https://stackoverflow.com/questions/31357353/using-cloudfront-with-django-s3boto
 
-class MediaStorage(S3Boto3Storage):
-    location = 'media'
+# class MediaStorage(S3Boto3Storage):
+#     location = 'media'
 
 
-class StaticStorage(S3Boto3Storage):
-    location = 'staticfile'
+# class StaticStorage(S3Boto3Storage):
+#     location = 'staticfile'
 
 
-# class StaticStorage(S3BotoStorage):
-# """uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
-#     location = settings.STATICFILES_LOCATION
+class StaticStorage(S3BotoStorage):
+"""uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
+    location = settings.STATICFILES_LOCATION
 
-#     def __init__(self, *args, **kwargs):
-#         kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
-#         super(StaticStorage, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_S3_CUSTON_DOMAIN
+        super(StaticStorage, self).__init__(*args, **kwargs)
 
-# class MediaStorage(S3BotoStorage):
-# """uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
-#     location = settings.MEDIAFILES_LOCATION
+class MediaStorage(S3BotoStorage):
+"""uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
+    location = settings.MEDIAFILES_LOCATION
 
-#     def __init__(self, *args, **kwargs):
-#         kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
-#         super(MediaStorage, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_S3_CUSTON_DOMAIN
+        super(MediaStorage, self).__init__(*args, **kwargs)
