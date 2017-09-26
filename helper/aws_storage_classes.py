351678c9020aab4d610e8d899a1fb89cf92ff3cd
Fabiano Almeida <fabiano@znc.com.br>
2017-09-25 23:13:47 -0300

    ref #40 erro de identação

--- helper/aws_storage_classes.py
+++ helper/aws_storage_classes.py
@@ -10,9 +10,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 # class StaticStorage(S3Boto3Storage):
 #     location = 'staticfile'
 
-
 class StaticStorage(S3BotoStorage):
-	"""uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
+    """uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
     location = 'staticfile'
 
     def __init__(self, *args, **kwargs):
@@ -20,7 +19,7 @@ class StaticStorage(S3BotoStorage):
         super(StaticStorage, self).__init__(*args, **kwargs)
 
 class MediaStorage(S3BotoStorage):
-	"""uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
+    """uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
     location = 'media'
 
     def __init__(self, *args, **kwargs):
