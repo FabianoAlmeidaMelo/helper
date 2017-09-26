82d9fbeb9ebde339765855db462ddbc1a18f8e71
Fabiano Almeida <fabiano@znc.com.br>
2017-09-25 23:19:10 -0300

    ref #40 'S3Boto3Storage' essa é a classe pai

--- helper/aws_storage_classes.py
+++ helper/aws_storage_classes.py
@@ -10,7 +10,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 # class StaticStorage(S3Boto3Storage):
 #     location = 'staticfile'
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     """uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
     location = 'staticfile'
 
@@ -18,7 +18,7 @@ class StaticStorage(S3BotoStorage):
         kwargs['custom_domain'] = settings.AWS_S3_CUSTON_DOMAIN
         super(StaticStorage, self).__init__(*args, **kwargs)
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     """uploads to 'mybucket/media/', serves from 'cloudfront.net/media/'"""
     location = 'media'
 
