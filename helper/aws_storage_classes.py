f81d5b0f6f6355bb6ba32eb6c9226363d8ebbe6c
Fabiano Almeida <fabiano@znc.com.br>
2017-09-25 23:32:06 -0300

    ref #40 limpeza de #codigos_comentados_desnecessarios

--- helper/aws_storage_classes.py
+++ helper/aws_storage_classes.py
@@ -1,14 +1,6 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-# ???https://stackoverflow.com/questions/31357353/using-cloudfront-with-django-s3boto
-
-# class MediaStorage(S3Boto3Storage):
-#     location = 'media'
-
-
-# class StaticStorage(S3Boto3Storage):
-#     location = 'staticfile'
 
 class StaticStorage(S3Boto3Storage):
     """uploads to 'mybucket/staticfile/', serves from 'cloudfront.net/staticfile/'"""
